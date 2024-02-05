3f92ba184e0889b9812f1e61c6f9c622a203f309
Himanshu Saini <himanshusaini0612@gmail.com>
2024-02-05 10:04:24 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const dbConfig = require("./config/dbConfig");
+require("dotenv").config();
+
+dbConfig();
+const usersRoute = require("./routes/usersRoutes");
+const examsRoute = require("./routes/examsRoutes");
+const reportsRoute = require("./routes/reportsRoutes");
+const courseRoute = require("./routes/courseRoutes");
+const universityRoute = require("./routes/universityRoutes");
+
+const app = express();
+app.use(express.json());
+
+// Avaliable Routes
+app.use("/api/user", usersRoute);
+app.use("/api/exam", examsRoute);
+app.use("/api/report", reportsRoute);
+app.use("/api/course", courseRoute);
+app.use("/api/university", universityRoute);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`The server is listening on ${PORT}`);
+});
